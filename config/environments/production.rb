fc1dbed35a3d4a6dfe78dcd4be693b4a58b0c6c2
Rachel Williams <williamsrachelada@gmail.com>
2024-08-30 16:34:39 -0700

    Add devise files from install and update env configuration to use devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,9 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  #
+  # TODO: Setup for Devise once prod is available
+  # config.action_mailer.default_url_options = { host: set_to_host_of_app }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
