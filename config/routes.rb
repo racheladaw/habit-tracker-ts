b87bdd22b208fd1c831ce45a29582b79f82108a9
Rachel Williams <williamsrachelada@gmail.com>
2024-09-11 16:59:18 -0700

    Add Users Controller and route for accessing current_user from React

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
+
+  namespace :api do
+    namespace :v1 do
+      resource :user, only: [:show]
+    end
+  end
 end
